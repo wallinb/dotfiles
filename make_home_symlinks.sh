1fe6e634ef96b45258c7bc843660f350b0e8c6bf
Bruce Wallin <bruce.wallin@colorado.edu>
2015-03-19 17:01:33 -0600

    Adds readline inputrc and bash aliases.

--- make_home_symlinks.sh
+++ make_home_symlinks.sh
@@ -1,3 +1,7 @@
+ln -s ~/configs/readline/inputrc ~/.inputrc
 ln -s ~/configs/tmux/tmux.conf ~/.tmux.conf
+
 ln -s ~/configs/vim/vimrc ~/.vimrc
-ln -s ~/configs/tmux/bashrc_linux ~/.bashrc
+
+ln -s ~/configs/bash/bashrc_linux ~/.bashrc
+ln -s ~/configs/bash/bash_aliases ~/.bash_aliases
