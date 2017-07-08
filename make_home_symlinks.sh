620c2ec81c7b458b47f8e50c8efdeb26ee15ee91
Bruce Wallin <bruce.wallin@nsidc.org>
2017-07-08 16:54:22 -0600

    Split bashrc by platform

--- make_home_symlinks.sh
+++ make_home_symlinks.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 ln -s ~/dotfiles/readline/inputrc ~/.inputrc
+
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux/.tmux-osx.conf ~/.tmux-osx.conf
 
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+
 mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
-# ln -s ~/dotfiles/bash/bashrc_linux ~/.bashrc
-ln -s ~/dotfiles/bash/bashrc_mac ~/.bashrc
+ln -s ~/dotfiles/bash/bashrc_linux ~/.bashrc_linux
+ln -s ~/dotfiles/bash/bashrc_osx ~/.bashrc_osx
 ln -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 
 ln -s ~/dotfiles/flake8 ~/.config/flake8
