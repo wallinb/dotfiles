155a6fe95a1fc005a7c4c1fb22a5393ada1ecf0b
Bruce Wallin <bruce.wallin@nsidc.org>
2019-04-15 23:43:53 -0600

    Add setup input script

---
+++ bin/setup_input.sh
@@ -0,0 +1,10 @@
+touchpad="SynPS/2 Synaptics TouchPad" # Turn off tapping drag
+xinput set-prop "$touchpad"  "libinput Tapping Drag Enabled" 0
+# Turn on natural scrolling
+xinput set-prop "$touchpad"  "libinput Natural Scrolling Enabled" 1
+
+# Set keyboard layout and options
+setxkbmap -model pc105 -layout us -variant dvorak -option ctrl:nocaps
+
+# Detect monitor setup
+autorandr --change
